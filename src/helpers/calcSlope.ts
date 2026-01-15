c81f7ffc8d27a44d4d1285d883e39afe41ae926e
tjjoris <tjjoris@gmail.com>
2026-01-15 12:45:38 -0500

    added comment header to CalcSlope function

--- src/helpers/calcSlope.ts
+++ src/helpers/calcSlope.ts
@@ -1,4 +1,10 @@
 import * as THREE from 'three';
+/**
+ * calculate the slope from 2 points.
+ * @param pos1 
+ * @param pos2 
+ * @returns 
+ */
 export default function CalcSlope(pos1: THREE.Vector2, pos2: THREE.Vector2): number {
     return (pos2.y - pos1.y) / (pos2.x - pos1.x);
 }
