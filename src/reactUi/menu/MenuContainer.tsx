5cbf6cf03c5115384581cf6f0fc71d0c0b54184c
tjjoris <tjjoris@gmail.com>
2026-01-30 13:26:12 -0500

    isVerticalInverted and toggleVerticalInverted are states and props passed to menuScreen, and live on menuContainer

--- src/reactUi/menu/MenuContainer.tsx
+++ src/reactUi/menu/MenuContainer.tsx
@@ -4,14 +4,21 @@ import MenuScreen from "./MenuScreen";
 
 export default function MenuContainer() {
     const [isOpen, setIsOpen] = useState(false);
+    const [isVerticalInverted, setIsVerticalInverted] = useState(true);
 
     const toggleMenu = () => {
         setIsOpen(!isOpen);
     };
 
+    const toggleInvertVertical = () => {
+        setIsVerticalInverted(!isVerticalInverted);
+    };
     return (<>
         {isOpen ?
-            <MenuScreen toggleMenu={toggleMenu} />
+            <MenuScreen
+                toggleMenu={toggleMenu}
+                isVerticalInverted={isVerticalInverted}
+                toggleInvertVertical={toggleInvertVertical} />
             : <MenuHamburger toggleMenu={toggleMenu} />}
     </>);
 }
--- src/reactUi/menu/MenuScreen.tsx
+++ src/reactUi/menu/MenuScreen.tsx
@@ -1,7 +1,15 @@
 import React from "react";
 import MenuButton from "./MenuButton";
 
-export default function MenuScreen({ toggleMenu }: { toggleMenu: () => void }) {
+export default function MenuScreen({
+    toggleMenu,
+    isVerticalInverted,
+    toggleInvertVertical
+}: {
+    toggleMenu: () => void,
+    isVerticalInverted: boolean,
+    toggleInvertVertical: () => void
+}) {
     return (<>
         <div className="menu-screen">
             <MenuButton name="Close Menu" onClick={toggleMenu} />
