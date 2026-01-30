01f61b665ac48a2b773b0aed62d3f0d76c4bebe4
tjjoris <tjjoris@gmail.com>
2026-01-30 13:21:06 -0500

    menu screen has close menu button which closes the menu screen.

---
+++ src/reactUi/menu/MenuButton.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function MenuButton({
+    name,
+    onClick
+}: {
+    name: string;
+    onClick: () => void;
+}) {
+    return (
+        <button
+            className="menu-button"
+            onClick={onClick}
+        >
+            {name}
+        </button>
+    )
+}
--- src/reactUi/menu/MenuScreen.tsx
+++ src/reactUi/menu/MenuScreen.tsx
@@ -1,7 +1,10 @@
 import React from "react";
+import MenuButton from "./MenuButton";
 
 export default function MenuScreen({ toggleMenu }: { toggleMenu: () => void }) {
-    return (
-        <div className="menu-screen">    </div>
-    )
+    return (<>
+        <div className="menu-screen">
+            <MenuButton name="Close Menu" onClick={toggleMenu} />
+        </div>
+    </>)
 }
