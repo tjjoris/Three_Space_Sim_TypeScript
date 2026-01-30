a42808b1bf397819d47b53c550903e3c13fd1dd0
tjjoris <tjjoris@gmail.com>
2026-01-30 15:17:57 -0500

    added comment headers for cookie helper, and fixed the name for key to value for the cookie value in set cookie.

--- src/helpers/cookieHandler.ts
+++ src/helpers/cookieHandler.ts
@@ -1,18 +1,36 @@
-export function setCookie(name: string, key: string, daysToExpire: number) {
+/**
+ * sets a cookie for a key value pair expiring in days.
+ * @param name the key name
+ * @param value the value 
+ * @param daysToExpire days to expire from today.
+ */
+export function setCookie(name: string, value: string, daysToExpire: number) {
     const date = new Date();
     date.setDate(date.getDate() + (daysToExpire));
     const expires: string = "expires=" + date.toUTCString();
-    document.cookie = `${name}=${key}; ${expires}; path=/`;
+    document.cookie = `${name}=${value}; ${expires}; path=/`;
 }
 
+/**
+ * logs the cookies.
+ */
 export function logCookie() {
     console.log(document.cookie);
 }
 
+/**
+ * deletes the cookie for the key.
+ * @param name the key name for the cookie to delete.
+ */
 export function deleteCookie(name: string) {
     setCookie(name, "", -1);
 }
 
+/**
+ * return the value for the key of the cookie.
+ * @param name the key name for the cookie
+ * @returns the value of the key.
+ */
 export function getCookie(name: string): string | null {
     const nameEQ = encodeURIComponent(name) + '=';
     const cookies = document.cookie.split('; ');
