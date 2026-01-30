be08cdffd391d1fd7ab123445efb76a5ab05862d
tjjoris <tjjoris@gmail.com>
2026-01-30 13:04:58 -0500

    hamburger button calls toggle menu in menu container component and changes the button.

--- src/reactUi/menu/MenuContainer.tsx
+++ src/reactUi/menu/MenuContainer.tsx
@@ -1,6 +1,12 @@
-import React from "react";
+import React, { useState } from "react";
 import MenuHamburger from "./MenuHamburger";
 
 export default function MenuContainer() {
-    return <MenuHamburger isOpen={true} />;
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = () => {
+        setIsOpen(!isOpen);
+    };
+
+    return <MenuHamburger isOpen={isOpen} toggleMenu={toggleMenu} />;
 }
--- src/reactUi/menu/MenuHamburger.tsx
+++ src/reactUi/menu/MenuHamburger.tsx
@@ -1,5 +1,20 @@
 import React from 'react';
 
-export default function MenuHamburger({ isOpen }: { isOpen: boolean }) {
-    return (isOpen ? <p className="menu-hamburger">Menu Open</p> : <p>Menu Closed</p>);
+export default function MenuHamburger({
+    isOpen,
+    toggleMenu
+}: {
+    isOpen: boolean;
+    toggleMenu: () => void;
+}) {
+    return (!isOpen ?
+        <button
+            className="menu-hamburger"
+            onClick={toggleMenu}
+        >Menu closed</button> :
+        <button className="menu-hamburger">Menu Open</button>);
 }
+
+
+
+
