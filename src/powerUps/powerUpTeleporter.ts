68ab6bc9806c80a76171423db8f3b691ae8f2e39
tjjoris <tjjoris@gmail.com>
2026-02-13 12:06:55 -0500

    made teleportPoerUp an exported function instead of exisint inside a class

--- src/powerUps/powerUpTeleporter.ts
+++ src/powerUps/powerUpTeleporter.ts
@@ -2,34 +2,24 @@ import Mover from "../ship/movement/mover";
 import * as THREE from "three";
 import type PowerUp from "./powerUp";
 
+
 /**
- * teleports the power up to in front of the player mover
+ * teleports the passed powerup in front of the passed player mover.
+ * @param powerUp 
  */
-export default class PowerUpTeleporter {
-    playerMover: Mover;
-
-    constructor(mover: Mover) {
-        this.playerMover = mover;
-    }
-
-    /**
-     * teleports the passed powerup in front of the player mover.
-     * @param powerUp 
-     */
-    teleportPowerUp(powerUp: PowerUp) {
-        const localForward = new THREE.Vector3();
-        this.playerMover.getWorldDirection(localForward);
-        const invertedLocalForward = localForward.clone().negate();
-        const distanceInFront = (Math.random() * 3) + 4//expectedDistanceInFront + (Math.random() * 1);
-        const randomOffset = new THREE.Vector3(
-            (Math.random() - 0.5) * 3,
-            (Math.random() - 0.5) * 3,
-            (Math.random() - 0.5) * 3
-        );
-        //pos is a temporary position set to where the powerup will move.
-        let pos = new THREE.Vector3();
-        pos.copy(this.playerMover.position).add(invertedLocalForward.multiplyScalar(distanceInFront)).add(randomOffset);
-        //have the powerup class copy the position.
-        powerUp.copyPos(pos);
-    }
+export default function teleportPowerUp(powerUp: PowerUp, playerMover: Mover) {
+    const localForward = new THREE.Vector3();
+    playerMover.getWorldDirection(localForward);
+    const invertedLocalForward = localForward.clone().negate();
+    const distanceInFront = (Math.random() * 3) + 4//expectedDistanceInFront + (Math.random() * 1);
+    const randomOffset = new THREE.Vector3(
+        (Math.random() - 0.5) * 3,
+        (Math.random() - 0.5) * 3,
+        (Math.random() - 0.5) * 3
+    );
+    //pos is a temporary position set to where the powerup will move.
+    let pos = new THREE.Vector3();
+    pos.copy(playerMover.position).add(invertedLocalForward.multiplyScalar(distanceInFront)).add(randomOffset);
+    //have the powerup class copy the position.
+    powerUp.copyPos(pos);
 }
