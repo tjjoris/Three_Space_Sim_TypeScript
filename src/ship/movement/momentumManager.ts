243b69a0851dedf14686a94dfe702308306f377b
tjjoris <tjjoris@gmail.com>
2026-02-24 09:19:44 -0500

    main MomentumManager functionality implemented, not tested.

--- src/ship/movement/momentumManager.ts
+++ src/ship/movement/momentumManager.ts
@@ -2,28 +2,67 @@ import * as THREE from "three";
 import Mover from "./mover";
 
 /**
- * uses calculateAcceleration3d to calculate the world acceleration.
- * has it's thrust values passed for each axis.
- * then applies these based on the mover, saving the world acceleration.
- * the getter then gets the world acceleration to be used by the mover.
+ * stores the world momentum.
+ * then calculates the relative acceleration.
+ * then gets the relative speed to be used by the mover.
  */
 export default class MomentumManager {
-    private acceleration3d: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    private worldSpeed: THREE.Vector3 = new THREE.Vector3(0, 0, 0);
+    private massCoefficient: number;
 
-
-    // setVerticalAxis(value: number) {
-    //     this.verticalAxisValue = value;
-    // }
-    // setHorizontalAxis(value: number) {
-    //     this.horizontalAxisValue = value;
-    // }
-    // setForwardAxis(value: number) {
-    //     this.horizontalAxisValue = value;
-    // }
-
-    calculateAcceleration3d(verticalThrust: number, horizontalThrust: number, forwardThrust: number, mover: Mover): THREE.Vector3 {
-        return new THREE.Vector3(0, 0, 0);
+    constructor(mass: number) {
+        this.massCoefficient = mass;
     }
 
+
+    /**
+     * calculate the relative speed.
+     * @param verticalThrust 
+     * @param horizontalThrust 
+     * @param forwardThrust 
+     * @param mover 
+     * @returns 
+     */
+    calculateRelativeSpeed(verticalThrust: number, horizontalThrust: number, forwardThrust: number, mover: Mover): THREE.Vector3 {
+        const relativeAcceleration = new THREE.Vector3(horizontalThrust, verticalThrust, forwardThrust);
+        const worldAccel = this.calculateWorldDirFromLocal(relativeAcceleration, mover as THREE.Object3D);
+        const worldSpeed = this.applyWorldAcceleration(worldAccel, this.worldSpeed, this.massCoefficient);
+        return this.calculateLocalDirectionFromWorld(worldSpeed, mover as THREE.Object3D);
+
+    }
+
+    /**
+     * calculate the world speed.
+     * @param relativeDirection 
+     * @param object 
+     * @returns 
+     */
+    calculateWorldDirFromLocal(relativeDirection: THREE.Vector3, object: THREE.Object3D): THREE.Vector3 {
+        // const newV3 = new THREE.Vector3();
+        // object.getWorldDirection(newV3);
+        // const returnV3 = new THREE.Vector3();
+        // returnV3.copy(newV3).add(relativeAcceleration);
+        // return returnV3;
+        const worldDir = object.localToWorld(new THREE.Vector3().copy(relativeDirection));
+        return worldDir;
+    }
+
+    /**
+     * apply world acceleration
+     */
+    applyWorldAcceleration(worldAcceleration: THREE.Vector3, worldSpeed: THREE.Vector3, massCoefficient: number): THREE.Vector3 {
+        const x = worldSpeed.x + (massCoefficient * worldAcceleration.x);
+        const y = worldSpeed.y + (massCoefficient * worldAcceleration.y);
+        const z = worldSpeed.z + (massCoefficient * worldAcceleration.z);
+        return new THREE.Vector3(x, y, z);
+    }
+
+    /**
+     * calculate relative speed from world speed.
+     */
+    calculateLocalDirectionFromWorld(worlDirection: THREE.Vector3, object: THREE.Object3D): THREE.Vector3 {
+        const localDir = object.worldToLocal(new THREE.Vector3().copy(worlDirection));
+        return localDir;
+    }
 
 }
