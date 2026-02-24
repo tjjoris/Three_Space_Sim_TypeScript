568d2e9561f460893ecd33e5a7f53dd144ecde0c
tjjoris <tjjoris@gmail.com>
2026-02-24 07:55:39 -0500

    finished axialThrust class, it needs to be tested.

--- src/axes/axialLerp.ts
+++ src/axes/axialLerp.ts
@@ -1,5 +1,12 @@
 import lerp from "../helpers/lerp";
 
+/**
+ * lerp function using axial outputs with a min of -1 and max of 1.
+ * @param min 
+ * @param max 
+ * @param axisValue 
+ * @returns 
+ */
 export default function axialLerp(min: number, max: number, axisValue: number): number {
     const unitInteval = (axisValue * 0.5) + 0.5;
     return lerp(min, max, unitInteval);
--- src/ship/movement/axialThrust.ts
+++ src/ship/movement/axialThrust.ts
@@ -3,7 +3,7 @@
  * desired thrust known from axis value and max thrust. the rate is controlled by max increase and decrease rates.
  */
 
-import lerp from "../../helpers/lerp";
+import axialLerp from "../../axes/axialLerp"
 export default class AxialThrust {
     private thrust: number = 0;
     private maxThrust: number;
@@ -19,10 +19,33 @@ export default class AxialThrust {
     /**
      * calculate the thrust based on the axis value, delta time, thrust rate, and max thrust.
      * @param axisValue 
-     * @param deltaTimer 
+     * @param deltaTime 
      */
-    calculateThrust(axisValue: number, deltaTimer: number): number {
-
-        return 0;
+    calculateThrust(axisValue: number, deltaTime: number): number {
+        const desiredThrust = axialLerp(- this.maxThrust, this.maxThrust, axisValue);
+        //change thrust rate
+        if (desiredThrust > this.thrust) {
+            return this.increaseThrust(this.thrust, desiredThrust, this.maxIncreaseRate, deltaTime);
+        }
+        if (desiredThrust < this.thrust) {
+            return this.reduceThrust(this.thrust, desiredThrust, this.maxDecreaseRate, deltaTime);
+        }
+        return this.thrust;
+    }
+    /**
+     * increase thrust
+     */
+    increaseThrust(currentThurst: number, desiredThrust: number, maxIncreaseRate: number, deltaTime: number): number {
+        let newThrust = currentThurst + (deltaTime * maxIncreaseRate);
+        newThrust = Math.max(newThrust, desiredThrust);
+        return newThrust;
+    }
+    /**
+     * reduce thrust
+     */
+    reduceThrust(currentThrust: number, desiredThrust: number, maxDecreaseRate: number, deltaTime: number): number {
+        let newThrust = currentThrust - (deltaTime * maxDecreaseRate);
+        newThrust = Math.min(newThrust, desiredThrust);
+        return newThrust;
     }
 }
