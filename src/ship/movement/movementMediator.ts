9cb3c16c6372d0441cb3b8f90ee7653acc3f7275
tjjoris <tjjoris@gmail.com>
2026-02-24 09:32:02 -0500

    implemented main functionality of MovementMediator, not yet tested.

--- src/ship/movement/movementMediator.ts
+++ src/ship/movement/movementMediator.ts
@@ -1,5 +1,7 @@
 import Mover from "./mover";
 import Axis from "../../axes/axis";
+import AxialThrust from "./axialThrust";
+import MomentumManager from "./momentumManager";
 
 /**
  * gets the input from the axes, and applies them to MomentumManager, then gets the acceleration from MomentumManager
@@ -7,23 +9,44 @@ import Axis from "../../axes/axis";
  * and applies it to mover.
  */
 export default class MovementMediator {
+
+    private momentumManager: MomentumManager;
     private mover: Mover;
     private verticalAxis: Axis;
+    private verticalAxialThrust: AxialThrust;
     private horizontalAxis: Axis;
+    private horizontalAxialThrust: AxialThrust;
     private forwardAxis: Axis;
+    private forwardAxialThrust: AxialThrust;
 
-    constructor(mover: Mover, verticalAxis: Axis, horizontalAxis: Axis, forwardAxis: Axis) {
+    constructor(momentumManager: MomentumManager, mover: Mover, verticalAxis: Axis, horizontalAxis: Axis, forwardAxis: Axis,
+        verticalAxialThrust: AxialThrust, horizontalAxialThrust: AxialThrust, forwardAxialThrust: AxialThrust
+    ) {
+        this.momentumManager = momentumManager;
         this.mover = mover;
         this.verticalAxis = verticalAxis;
         this.horizontalAxis = horizontalAxis;
         this.forwardAxis = forwardAxis;
+        this.verticalAxialThrust = verticalAxialThrust;
+        this.horizontalAxialThrust = horizontalAxialThrust;
+        this.forwardAxialThrust = forwardAxialThrust;
 
     }
 
     tick(deltaTime: number) {
-        // this.mover.tick(deltaTime);
+        //get axes inputs
         const vertical = this.verticalAxis.getValue();
         const horizontal = this.horizontalAxis.getValue();
         const forward = this.forwardAxis.getValue();
+        //find thrust values from axial thrusts.
+        const verticalThrust = this.verticalAxialThrust.calculateThrust(vertical, deltaTime);
+        const horizontalThrust = this.horizontalAxialThrust.calculateThrust(horizontal, deltaTime);
+        const forwardAxialThrust = this.forwardAxialThrust.calculateThrust(forward, deltaTime);
+        //apply axes inputs to momentum manager
+        const localSpeed = this.momentumManager.calculateRelativeSpeed(verticalThrust, horizontalThrust, forwardAxialThrust, this.mover);
+        //set velocity of mover
+        this.mover.setVelocity(localSpeed);
+        //tick mover
+        this.mover.tick(deltaTime);
     }
 }
