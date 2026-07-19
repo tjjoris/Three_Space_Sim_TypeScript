57eb5ee5fcc459f523ab65b888b7c50a066ec27c
tjjoris <tjjoris@gmail.com>
2026-07-19 13:32:08 -0400

    added class properties and getters for joy.ts.

---
+++ src/input/joy.ts
@@ -0,0 +1,36 @@
+/*
+ joy.ts
+ @Author:Luke Johnson
+ this class is used for tracking the joyId to be used by the binding, for reading joy inputs.
+ the name is set from cookies, and when a joy is connected, the connected joys name is
+ comparted to the names from these joys, to see if they match. If they do, and the joy is not
+ enabled, the joy is enabled. but if the id's of the connected joy, and this joy do not match, and the names match, and this joy is not enabled, and the id of this joy is set to the id of the connected joy. note that it does this in order of the joys array, meaning lower id joys are checked first. meaning if multiple joys have the same name, the lower number joy ids from cookies, are used first when joys are connected.
+ */
+export default class Joy{
+	private joyId:number|null;
+	private joyName:string|null;
+	private enabled:boolean;
+
+	public constructor(
+		joyId:number|null,
+		joyName:string|null,
+		enabled:boolean
+	)
+	{
+		this.joyId = joyId;
+		this.joyName = joyName;
+		this.enabled = enabled;
+	}
+
+	public getJoyId():number|null {
+		return this.joyId;
+	}
+
+	public getJoyName():string|null {
+		return this.joyName;
+	}
+
+	public getEnabled():boolean{
+		return this.enabled;
+	}
+}
