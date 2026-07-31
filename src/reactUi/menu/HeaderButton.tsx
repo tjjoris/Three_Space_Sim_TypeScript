16fa2b90f7be0a83f52f71a0daf1ecfdad26b271
tjjoris <tjjoris@gmail.com>
2026-07-31 07:03:49 -0400

    changed menuScreen to have a function to determine the z-index of header buttons and pass it to the header button.

--- src/reactUi/menu/HeaderButton.tsx
+++ src/reactUi/menu/HeaderButton.tsx
@@ -8,17 +8,17 @@ type Props = {
     name: MenuTabsType;
     action: (tabName: MenuTabsType) => void;
     currentTab: MenuTabsType;
+    zIndex: number;
 }
 //
 export default function HeaderButton(props: Props) {
-	//if passed props for tab matches name, set tag to put in front.
-	let tag = "unselected-header-button"  
-	if (props.currentTab === props.name) {
-		tag = "selected-header-button"
-	}		
+			
     return (
         <>
-            <button className={`menu-header-button ${tag}`} onClick={
+            <button 
+	    className='menu-header-button' 
+	    style={{zIndex: props.zIndex}}
+	    onClick={
 	    () => 
 		  props.action(props.name)
 	    }> 
--- src/reactUi/menu/MenuScreen.tsx
+++ src/reactUi/menu/MenuScreen.tsx
@@ -4,7 +4,7 @@ import InfoMenuContent from "./InfoMenuContent";
 import KeybindsMenuContent from "./KeybindsMenuContent";
 import HeaderButton from "./HeaderButton";
 import MenuButton from "./MenuButton";
-import { useState} from "react";
+import { useState, useRef} from "react";
 export default function MenuScreen({
     toggleMenu,
 }: {
@@ -13,6 +13,44 @@ export default function MenuScreen({
 
 	//tabs state for different menu screens.
 	const [menuTab, setMenuTab] = useState<MenuTabsType>("info"); 
+	const zIndex = useRef(100);
+	const headerTabPassedTop= useRef(false);
+	const tabs:MenuTabsType[] = ['info', 'keybinds', 'settings'];
+
+	function retrunHeaderButtons(): React.ReactElement {
+		let headerButtons: React.ReactElement[] = [];
+		let localHeaderTabPassedTop: boolean = false;
+		let localZIndex:number = 100;
+		for (let headerButtonIndex:number = 0; headerButtonIndex < tabs.length; headerButtonIndex ++) {
+			if (localHeaderTabPassedTop == true) {
+				localZIndex --;
+			}
+			if (tabs[headerButtonIndex] == menuTab) {
+				localHeaderTabPassedTop = true;
+			}
+			headerButtons.push(
+				<HeaderButton name={tabs[headerButtonIndex]} action={gotoMenuScreen} currentTab={menuTab} zIndex={localZIndex} />);
+			
+		}
+		return (<>
+				{headerButtons}
+			</>);
+
+	}
+	/*
+	 *this function returns the header button, but sets the z-index based on if it is in front or behind a pervious one.
+	 */
+	function returnHeaderButton(nameParam:MenuTabsType, currentTabParam:MenuTabsType): React.ReactElement{
+		if (headerTabPassedTop.current == true) {
+			zIndex.current = zIndex.current - 1;
+		}
+		if (nameParam == currentTabParam) { 
+			headerTabPassedTop.current = true;
+		}
+		return (
+			<HeaderButton name={nameParam} action={gotoMenuScreen} currentTab={currentTabParam} zIndex={zIndex.current} />);
+
+	}
 	
 	//function to navigate to menu screen on tab click
 //the arrow syntax is more common in react and no {} are needed because it is only calling one function.
@@ -24,9 +62,13 @@ export default function MenuScreen({
         <div className="menu-screen">
 
             <div className="menu-header">
-		<HeaderButton name="info" action={gotoMenuScreen} currentTab ={menuTab} />
-		<HeaderButton name="keybinds" action={gotoMenuScreen} currentTab={menuTab} />
-		<HeaderButton name="settings" action={gotoMenuScreen} currentTab={menuTab} />
+	    	{retrunHeaderButtons()}
+		{/*	    	{tabs.map(tab => returnHeaderButton(tab, menuTab))}
+				*/}
+		{/*//		<HeaderButton name="info" action={gotoMenuScreen} currentTab ={menuTab} />
+//		<HeaderButton name="keybinds" action={gotoMenuScreen} currentTab={menuTab} />
+//		<HeaderButton name="settings" action={gotoMenuScreen} currentTab={menuTab} />
+//		*/}
             </div>
 		{menuTab === "info" &&
 			<InfoMenuContent />}
