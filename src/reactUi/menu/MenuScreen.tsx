a189af9b9ce5c6a713d6d11475c1730cb749e1f9
tjjoris <tjjoris@gmail.com>
2026-07-31 07:07:58 -0400

    cleaned up code and added comment header for new function.

--- src/reactUi/menu/MenuScreen.tsx
+++ src/reactUi/menu/MenuScreen.tsx
@@ -13,10 +13,14 @@ export default function MenuScreen({
 
 	//tabs state for different menu screens.
 	const [menuTab, setMenuTab] = useState<MenuTabsType>("info"); 
-	const zIndex = useRef(100);
-	const headerTabPassedTop= useRef(false);
 	const tabs:MenuTabsType[] = ['info', 'keybinds', 'settings'];
 
+
+	/*
+	 *function for returning all header buttons. 
+	 this function loops through all header buttons, it stores the z-index and if the button is the current one. if it passes the current one, it lowers the z-index by 1.
+	 @TODO: make this a seperate component.
+	 */
 	function retrunHeaderButtons(): React.ReactElement {
 		let headerButtons: React.ReactElement[] = [];
 		let localHeaderTabPassedTop: boolean = false;
@@ -37,21 +41,7 @@ export default function MenuScreen({
 			</>);
 
 	}
-	/*
-	 *this function returns the header button, but sets the z-index based on if it is in front or behind a pervious one.
-	 */
-	function returnHeaderButton(nameParam:MenuTabsType, currentTabParam:MenuTabsType): React.ReactElement{
-		if (headerTabPassedTop.current == true) {
-			zIndex.current = zIndex.current - 1;
-		}
-		if (nameParam == currentTabParam) { 
-			headerTabPassedTop.current = true;
-		}
-		return (
-			<HeaderButton name={nameParam} action={gotoMenuScreen} currentTab={currentTabParam} zIndex={zIndex.current} />);
 
-	}
-	
 	//function to navigate to menu screen on tab click
 //the arrow syntax is more common in react and no {} are needed because it is only calling one function.
 	const gotoMenuScreen = (screenName: MenuTabsType) => {
@@ -63,12 +53,6 @@ export default function MenuScreen({
 
             <div className="menu-header">
 	    	{retrunHeaderButtons()}
-		{/*	    	{tabs.map(tab => returnHeaderButton(tab, menuTab))}
-				*/}
-		{/*//		<HeaderButton name="info" action={gotoMenuScreen} currentTab ={menuTab} />
-//		<HeaderButton name="keybinds" action={gotoMenuScreen} currentTab={menuTab} />
-//		<HeaderButton name="settings" action={gotoMenuScreen} currentTab={menuTab} />
-//		*/}
             </div>
 		{menuTab === "info" &&
 			<InfoMenuContent />}
