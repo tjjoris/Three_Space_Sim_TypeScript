03661ef52fa4847584a23b2d280896cf1cef5e79
tjjoris <tjjoris@gmail.com>
2026-08-01 15:29:45 -0400

    added content to inputDiffsCompareForAlljoys. Added properties, constructor, and joyConnected.

--- src/input/inputDiffsCompareForAllJoys.ts
+++ src/input/inputDiffsCompareForAllJoys.ts
@@ -8,6 +8,30 @@ Also it's update loop is disabled in a similar way.
 when the update loop gets a valid diff, it checks if it is different from the previous stored diff, and if it is not, it stores it, and that state sends a signal to UseSyncExternalStore in React which updates the Menu showing the set binding.
 
  */
+import InputDiffsCompareForJoy from "./inputDiffsCompareForJoy";
+import JoyAxisInputDiffValueReporter from "./joyAxisInputDiffValueReporter";
+
 export default class InputDiffsCompareForAllJoys {
-		
+	//the array of joy input diffs.
+	private joys:InputDiffsCompareForJoy[];
+	//the reported axis input diff which can be null.
+	private inputReported:JoyAxisInputDiffValueReporter | null;
+
+	public constructor() {
+		this.joys = [];
+		this.inputReported = null;
+	}
+
+	/*
+	 * called when a joy is connected. creates an instqance of InputDiffsCompareForJoy for the joy, and that creates JoyAxisInputDiffValueReporters for that joys axes.
+	 */
+	joyConnected(joyId: number, joyName: string, axisCount: number) {
+		const joyDiff = new InputDiffsCompareForJoy(joyId, joyName, axisCount);
+		this.joys.push(joyDiff);
+	}
+
+	/*
+	 * called when a joy is disconnected, calls disconnect function for joy diff, and removes this joy diff from the array.
+	 */
+	
 }
