4527fa02d1b79115b6d3f6730a0d34a40ff57b98
tjjoris <tjjoris@gmail.com>
2026-08-04 06:51:03 -0400

    moved header buttons into new menuHeader.tsx so that functionality is contained in it's own component.

---
+++ src/reactUi/menu/MenuHeader.tsx
@@ -0,0 +1,71 @@
+/*
+ *MenuHeader.tsx
+ @Author: Luke Johnson
+ this React component appears at the top of every menu screen. it contains the menu tab buttons.
+ It has the function for whtne the buttons are clicked as props, and also the the list of menu buttons.
+ */
+import { useState} from "react";
+import type {MenuTabsType} from "../../types/menuTabsType"
+import HeaderButton from "./HeaderButton";
+
+interface Props {
+	menuTab: MenuTabsType;
+	gotoMenuScreen: (screenName:MenuTabsType) => void ; 
+	setMenuTab: React.Dispatch<React.SetStateAction<MenuTabsType>>;
+}
+export default function MenuHeader (props:Props)  {
+
+
+	//tabs state for different menu screens.
+	const tabs:MenuTabsType[] = ['info', 'keybinds', 'settings'];
+
+
+	/*
+	 *function for returning all header buttons. 
+	 this function loops through all header buttons, it stores the z-index and if the button is the current one. if it passes the current one, it lowers the z-index by 1.
+	 */
+	function returnHeaderButtons(): React.ReactElement {
+		//this is the array of react elements to return.
+		let headerButtons: React.ReactElement[] = [];
+		//this is the boolean to check if the button is the current menu.
+		let localHeaderTabPassedTop: boolean = false;
+		//this is the actual z index used in the component it is set to 100 when it is the selected button, otherwise it is 1 if before the selected button, or 49 if after and lowers by 1.
+		let localZIndexToBeUsed:number = 1;
+		let zIndexTrackedForStacking = 49;
+		//loop all elements i the tabs array of MenuTabsType.
+		for (let headerButtonIndex:number = 0; headerButtonIndex < tabs.length; headerButtonIndex ++) {
+			//check if this is the button for the current tab 
+			if (tabs[headerButtonIndex] == props.menuTab) {
+				//set the boolean for tracking.
+				localHeaderTabPassedTop = true;
+				//set the z index to appear above the menu
+				localZIndexToBeUsed = 100;
+			}
+			else {
+				//set the z index behind the menu but with room to lower for additional tabs appearing behind this one.
+				localZIndexToBeUsed = zIndexTrackedForStacking;
+			}
+			//true if has already looped past the current button.
+			if (localHeaderTabPassedTop == true) {
+				zIndexTrackedForStacking --;
+			}
+			//add a header button to the array.
+			headerButtons.push(
+				<HeaderButton name={tabs[headerButtonIndex]} action={props.gotoMenuScreen} currentTab={props.menuTab} zIndex={localZIndexToBeUsed} />);
+			
+		}
+		//return the array of header buttons.
+		return (<>
+				{headerButtons}
+			</>);
+
+	}
+
+
+	//return the actual header buttons for this component.
+	return (
+		<div className = "menu-header">
+			{returnHeaderButtons()}
+		</div>
+	)
+}
--- src/reactUi/menu/MenuScreen.tsx
+++ src/reactUi/menu/MenuScreen.tsx
@@ -4,65 +4,20 @@
  this is the react menu screen for the main menu. it contains the header and all components.
  */
 import type {MenuTabsType} from "../../types/menuTabsType"
+import MenuHeader from "./MenuHeader";
 import SettingsMenuContent from "./SettingsMenuContent";
 import InfoMenuContent from "./InfoMenuContent";
 import KeybindsMenuContent from "./KeybindsMenuContent";
-import HeaderButton from "./HeaderButton";
 import MenuButton from "./MenuButton";
 import { useState} from "react";
+
 export default function MenuScreen({
     toggleMenu,
 }: {
     toggleMenu: () => void,
 }) {
-
-	//tabs state for different menu screens.
+	//menu tab state for storing menu tab your in.
 	const [menuTab, setMenuTab] = useState<MenuTabsType>("info"); 
-	const tabs:MenuTabsType[] = ['info', 'keybinds', 'settings'];
-
-
-	/*
-	 *function for returning all header buttons. 
-	 this function loops through all header buttons, it stores the z-index and if the button is the current one. if it passes the current one, it lowers the z-index by 1.
-	 @TODO: make this a seperate component.
-	 */
-	function retrunHeaderButtons(): React.ReactElement {
-		//this is the array of react elements to return.
-		let headerButtons: React.ReactElement[] = [];
-		//this is the boolean to check if the button is the current menu.
-		let localHeaderTabPassedTop: boolean = false;
-		//this is the actual z index used in the component it is set to 100 when it is the selected button, otherwise it is 1 if before the selected button, or 49 if after and lowers by 1.
-		let localZIndexToBeUsed:number = 1;
-		let zIndexTrackedForStacking = 49;
-		//loop all elements i the tabs array of MenuTabsType.
-		for (let headerButtonIndex:number = 0; headerButtonIndex < tabs.length; headerButtonIndex ++) {
-			//check if this is the button for the current tab 
-			if (tabs[headerButtonIndex] == menuTab) {
-				//set the boolean for tracking.
-				localHeaderTabPassedTop = true;
-				//set the z index to appear above the menu
-				localZIndexToBeUsed = 100;
-			}
-			else {
-				//set the z index behind the menu but with room to lower for additional tabs appearing behind this one.
-				localZIndexToBeUsed = zIndexTrackedForStacking;
-			}
-			//true if has already looped past the current button.
-			if (localHeaderTabPassedTop == true) {
-				zIndexTrackedForStacking --;
-			}
-			//add a header button to the array.
-			headerButtons.push(
-				<HeaderButton name={tabs[headerButtonIndex]} action={gotoMenuScreen} currentTab={menuTab} zIndex={localZIndexToBeUsed} />);
-			
-		}
-		//return the array of header buttons.
-		return (<>
-				{headerButtons}
-			</>);
-
-	}
-
 	//function to navigate to menu screen on tab click
 //the arrow syntax is more common in react and no {} are needed because it is only calling one function.
 	const gotoMenuScreen = (screenName: MenuTabsType) => {
@@ -72,9 +27,7 @@ export default function MenuScreen({
     return (<>
         <div className="menu-screen">
 
-            <div className="menu-header">
-	    	{retrunHeaderButtons()}
-            </div>
+	<MenuHeader menuTab = {menuTab} gotoMenuScreen = {gotoMenuScreen} setMenuTab = {setMenuTab} />
 		{menuTab === "info" &&
 			<InfoMenuContent />}
 		{menuTab === "keybinds" &&
