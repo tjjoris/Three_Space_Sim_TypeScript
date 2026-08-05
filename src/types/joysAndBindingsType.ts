8f406c4b8ad38fe92bae80afc0feb8e1f11a55a8
tjjoris <tjjoris@gmail.com>
2026-08-05 00:06:55 -0400

    added flightAxisType.ts and implemented it in joysAndBindingsType.ts

--- src/types/joysAndBindingsType.ts
+++ src/types/joysAndBindingsType.ts
@@ -1,6 +1,7 @@
+import type {FlightAxisType}  from "./flightAxisType"
 /**
  *bindingsType.ts
  @Author: Luke Johnson
  * a type to store all bindings.
  */
-export type JoysAndBindingsType = {bindings: [{ flightAxis: string, refId: number, joyAxisId: number}], joys: [{refId: number, joyName: string, joyId: number}]}; 
+export type JoysAndBindingsType = {bindings: [{ flightAxis: FlightAxisType, refId: number, joyAxisId: number}], joys: [{refId: number, joyName: string, joyId: number}]}; 
