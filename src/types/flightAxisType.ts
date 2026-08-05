da79c96fc199360ccb61994ff0244125ed218556
tjjoris <tjjoris@gmail.com>
2026-08-04 23:58:37 -0400

    added flightAxisType.ts which stores flight axis strings for use by binding and joys type.

---
+++ src/types/flightAxisType.ts
@@ -0,0 +1,7 @@
+/**
+ *flightAxisType.ts
+ @Author: Luke Johnson
+ A type which can only be one of the following strings.
+ used for joys and bindings types.
+ */
+export type FlightAxisType = "pitch" | "roll" | "yaw" | "horizontal" | "vertical";
