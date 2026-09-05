c24b048a06fda88adee023ffc0dc11f5838c69d7
tjjoris <tjjoris@gmail.com>
2026-09-05 16:27:10 -0400

    created joysStorage class and initial class header.

---
+++ src/input/joysStorage.ts
@@ -0,0 +1,10 @@
+/*
+ * joysStorage.ts
+ * @Author: Luke Johnson
+ * this class stores all the connectable joys.
+ * it gets them from cookies or sets them from the bindings and the input diff joy connector.
+ */
+
+export default class JoysStorage {
+
+}
