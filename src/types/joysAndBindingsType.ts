bf8577177886c0b250d203563a5e97756ec85db2
tjjoris <tjjoris@gmail.com>
2026-09-19 17:33:39 -0400

    removed joysAndBindingsType.ts.

--- src/types/joysAndBindingsType.ts
+++
@@ -1,7 +0,0 @@
-import type {BindingsType} from "./bindingsType";
-/**
- *bindingsType.ts
- @Author: Luke Johnson
- * a type to store all bindings.
- */
-export type JoysAndBindingsType = {bindings: BindingsType, joys: [{refId: number, joyName: string, joyId: number}]}; 
