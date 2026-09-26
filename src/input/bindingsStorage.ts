aaa549d8035ffb2725b89e69cb4c2109455c5528
tjjoris <tjjoris@gmail.com>
2026-09-26 14:11:47 -0400

    added other functions for setting cookies for bindings and joys. input type, joy name, joy id, joy ref id. they have all been tested and work.

--- src/input/bindingsStorage.ts
+++ src/input/bindingsStorage.ts
@@ -10,6 +10,7 @@
 import JoyAxisBinding from "./joyAxisBinding";
 import type {BindingType } from "../types/bindingType";
 import type {FlightAxisType} from "../types/flightAxisType";
+import type {InputType} from "../types/inputType";
 import BindingsToStateConverter from "./bindingsToStateConverter";
 import GameState from "../ui/menu/gameState";
 import Joy from "./joy";
@@ -66,7 +67,15 @@ export default class BindingsStorage {
 		const expDays: number = 365;
 		//get the joyAxisBinding for this binding
 		const joyAxisBinding: JoyAxisBinding = this.bindingsRecord[flightAxis];
+		const joy: Joy | null = joyAxisBinding.getJoy();
+		if (joy == null) {
+			return;
+		}
 		this.setAxisNumberCookie(flightAxis, joyAxisBinding, expDays);
+		this.setInputTypeCookie(flightAxis, "joyAxis", expDays);
+		this.setJoyNameCookie(flightAxis, joy, expDays);
+		this.setJoyIdCookie(flightAxis, joy, expDays);
+		this.setJoyRefIdCookie(flightAxis, joy, expDays);
 	}
 
 	/*
@@ -78,11 +87,69 @@ export default class BindingsStorage {
 		//get the axis number id for the binding
 		const axisNumber: number | null = joyAxisBinding.getAxisId();
 		if (axisNumber == null) {
-			return; }
+			return;
+		}
 		//convert the axis number to a string
 		const axisNumString: string = axisNumber.toString();
 		//set the axis number to a cookie
 		setCookie(axisCookieKey, axisNumString, expDays);	
 	}
 
+	/*
+	 * set the input type cookie
+	 */
+	public setInputTypeCookie(flightAxis: FlightAxisType, inputType: InputType, expDays: number) {
+		//parse the cookie key for the input type
+		const inputTypeCookieKey: string = flightAxis + "Type";
+		//set the input type for the flight axis to a cookie
+		setCookie(inputTypeCookieKey, inputType, expDays);
+	}
+
+	/*
+	 * set joy name cookie
+	 */
+	public setJoyNameCookie(flightAxis: FlightAxisType, joy: Joy, expDays: number) {
+		//parse the cookie key for the joyName
+		const joyNameCookieKey: string = flightAxis + "JoyName"
+		//get the joy name string
+		const joyName: string | null = joy.getJoyName();
+		if (joyName == null) {
+			return;
+		}
+		//set the cookie for the joy name
+		setCookie(joyNameCookieKey, joyName, expDays);
+	}
+
+	/**
+	 *set the joy id(index) cookie
+	 */
+	public setJoyIdCookie(flightAxis: FlightAxisType, joy: Joy, expDays: number) {
+		//parse the cookie key for the joyIndex
+		const joyIndexCookieKey: string = flightAxis + "JoyIndex";
+		//get the joyId
+		const joyIdNumber: number | null = joy.getJoyId();
+		if (joyIdNumber == null) {
+			return;
+		}
+		const joyIdString: string = joyIdNumber.toString();
+		//set the cookie for the joyId(index)
+		setCookie(joyIndexCookieKey, joyIdString, expDays);
+	}
+
+	/*
+	 * set the joy ref id cookie
+	 */
+	public setJoyRefIdCookie(flightAxis: FlightAxisType, joy: Joy, expDays: number) {
+		//parse the cookie key for the joyRefId
+		const joyRefIdCookieKey: string = flightAxis + "JoyRefId";
+		//get the joyRefId
+		const joyRefIdNumber: number | null = joy.getJoyRefId();
+		if (joyRefIdNumber == null) {
+			return;
+		}
+		//set the joy ref id number to a string
+		const joyRefIdString: string = joyRefIdNumber.toString();
+		//set the cookie for the joy ref id
+		setCookie(joyRefIdCookieKey, joyRefIdString, expDays);
+	}
 }
---
+++ src/types/inputType.ts
@@ -0,0 +1,6 @@
+/*
+ *inputType.ts
+ @Author: Luke Johnson
+ this is a type for the inputs for bindings, so we know if a binidng is set to a joy axis, joy button, touch, keyboard button, or mouse.
+ */
+export type InputType = "joyAxis" | "touch"
